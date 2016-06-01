213a769a385d24f7c754b6119b66f6834b2bb5b2
maggie <maggie94gkn@gmail.com>
2016-06-01 13:00:34 -0700

    delete unnecessary files

--- Rich Internet Applications/Realtime Group-Chat application/webchat/db.js
+++
@@ -1,5 +0,0 @@
-var DataStore = require("nedb");
-var dbFile = new DataStore({filename:"meshdata.db", autoload:"true"});
-dbFile.ensureIndex({ fieldName: 'socket_id', unique: true });
-
-module.exports = dbFile;
--- Rich Internet Applications/Recipe Calculator/server/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./bin/node server.js
